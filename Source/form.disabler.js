dd20c1bbfe7b08f093485e547bd71c80741710cf
Yorick Sijsling <yoricksijsling@gmail.com>
2011-03-17 04:33:59 -0700

    Disable submit buttons too.

--- Source/form.disabler.js
+++ Source/form.disabler.js
@@ -19,9 +19,11 @@ Element.implement({
         var fn = this.retrieve('disableForm_submitEvent') || function(e) { e.stop(); };
         this.store('disableForm_submitEvent', fn);
         this.addEvent('submit', fn);
+        this.getElements('input[type=submit]').each(function(button) { button.set('disabled', true); });
     },
     enableForm: function() {
         var fn = this.retrieve('disableForm_submitEvent');
         this.removeEvent('submit', fn);
+        this.getElements('input[type=submit]').each(function(button) { button.set('disabled', false); });
     }
 });
